9c63d9d36f21773c65a075cee3d4203d2889b6b4
Dionmy Olivares <hoeru@outlook.com>
2025-01-14 16:12:01 -0500

    Using APOD to have a different background image for the site every day

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+'use strict';
+
+const api = 'https://api.nasa.gov/planetary/apod?api_key=';
+const apiKey = 'CgpgMC0Wtld60xf2JqkVIeqBDboBvHOmzlSVJIE9';
 const app = angular.module('app', []);
+
+// Gets Astronomy Picture of the Day on load
+app.controller('backgroundImageController', ($scope, $http) => {
+    $scope.bgImg = {};
+    $http.get(`${api}${apiKey}`).then((res) => {
+        $scope.bgImg = res.data;
+    });
+});
