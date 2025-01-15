6346812227540095dc494c8afa943f6176692683
leeeaton885 <leeeaton885@gmail.com>
2025-01-14 21:46:14 -0700

    Completed basic query

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ const app = angular.module('app', []);
 // Gets Astronomy Picture of the Day on load
 app.controller('backgroundImageController', ($scope, $http) => {
     $scope.bgImg = {};
-    $http.get(`${api}${apiKey}`).then((res) => {
-        $scope.bgImg = res.data;
-    });
+    // $http.get(`${api}${apiKey}`).then((res) => {
+    //     $scope.bgImg = res.data;
+    // });
+});
+
+const weatherApi = 'https://api.nasa.gov/insight_weather/?api_key=';
+
+app.controller('weatherController', ($scope, $http) => {
+    $scope.weather = {};
+    $http.get(`${weatherApi}DEMO_KEY&feedtype=json&ver=1.0`).then((res => {
+        let key = res.data.sol_keys.slice(-1)[0];
+        let data = res.data[key]
+        $scope.sol = key
+        $scope.season = data.Season;
+        $scope.at = data.AT.av;
+        $scope.hws = data.PRE.av;
+        $scope.pre = data.HWS.av;
+        $scope.wd = data.WD.most_common.compass_point;
+    }));
+    $scope.weather = "1";
 });
