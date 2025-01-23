48ad5b65c7062fbb494addbd60c19e1448c85557
michelle eaton <10803668@uvu.edu>
2025-01-23 12:53:10 -0700

    temp commit

--- components/controllers/roverImages.js
+++ components/controllers/roverImages.js
@@ -10,6 +10,14 @@ angular.module('roverImages', [])
                     $http.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${newQuery}/photos?sol=1000&api_key=${apiKey}`)
                     .then((response) => {
                         $scope.roverImages = response.data.photos;
+                        // for (let r in $scope.roverImages) {
+                        //     $http
+                        // }
+                        let imgUrl = $scope.roverImages[0].img_src;
+                        $http.get(imgUrl).then(function (response) {
+                            console.log("imageUrl get:")
+                            console.log(response.data);
+                        })
                         console.log("roverImages controller: ")
                         console.log($scope.roverImages);
                     });
