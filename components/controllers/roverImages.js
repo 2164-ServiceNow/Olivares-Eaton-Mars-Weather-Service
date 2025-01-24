4447da40ef5873a39e42e2532f919454e29b5311
michelle eaton <10803668@uvu.edu>
2025-01-23 17:20:57 -0700

    completed formatting

--- components/controllers/roverImages.js
+++ components/controllers/roverImages.js
@@ -7,19 +7,9 @@ angular.module('roverImages', [])
             }, function(newQuery) {
                 $scope.value = newQuery;
                 if(newQuery !== '') {
-                    $http.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${newQuery}/photos?sol=1000&api_key=${apiKey}`)
+                    $http.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/${newQuery}/photos?sol=2000&api_key=${apiKey}`)
                     .then((response) => {
                         $scope.roverImages = response.data.photos;
-                        // for (let r in $scope.roverImages) {
-                        //     $http
-                        // }
-                        let imgUrl = $scope.roverImages[0].img_src;
-                        $http.get(imgUrl).then(function (response) {
-                            console.log("imageUrl get:")
-                            console.log(response.data);
-                        })
-                        console.log("roverImages controller: ")
-                        console.log($scope.roverImages);
                     });
                 }
             });
